35613dee3460f7506673658a3b25b69a44ba3781
NguyenDangAnhTuanB24DCCC278 <tuanlorion102@gmail.com>
2025-10-21 09:16:01 +0700

    Create Home.tsx

---
+++ bai1/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchForm from '../components/SearchForm';
+
+const Home: React.FC = () => (
+  <div>
+    <h1>Ứng dụng thời tiết</h1>
+    <SearchForm />
+  </div>
+);
+
+export default Home;
+export{};
